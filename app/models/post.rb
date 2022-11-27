ebc672d3c24638075473cd5dabcaf2fcdfd632ac
Mutuma Gitonga <mgitonga150@gmail.com>
2022-11-27 19:12:39 +0300

    Completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+
+  validate :title_is_clickbait
+
+  def title_is_clickbait
+    if title != "You Won't Believe These True Facts"
+      errors.add(:title)
+    end
+  end
 end
